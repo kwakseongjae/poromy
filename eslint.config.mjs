43d38d6cd89218ba8519a30e542b22288a62c359
kwakseongjae <gkffhdnls13@gmail.com>
2025-04-06 01:50:31 +0900

    feat: Set up Prettier and ESLint configurations with TailwindCSS support and remove unnecessary css code lines

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    plugins: ['prettier', 'tailwindcss'],
+    rules: {
+      'prettier/prettier': 'error',
+      'tailwindcss/classnames-order': 'error',
+      'tailwindcss/enforces-negative-arbitrary-values': 'error',
+      'tailwindcss/enforces-shorthand': 'error',
+      'tailwindcss/migration-from-tailwind-2': 'error',
+      'tailwindcss/no-custom-classname': 'warn',
+      'tailwindcss/no-contradicting-classname': 'error',
+      'tailwindcss/no-redundant-arbitrary-value': 'error',
+    },
+  },
 ];
 
 export default eslintConfig;
