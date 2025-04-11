d159aa7c06a0f647a81fc84be48cab0f710cd7e3
kwakseongjae <gkffhdnls13@gmail.com>
2025-04-11 18:22:55 +0900

    feat: Create index file for exporting SVG and image assets

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+// SVG
+export { default as LogoIcon } from '../../public/svg/logo-icon.svg'
+export { default as ChevronIcon } from '../../public/svg/chevron-icon.svg'
+
+// Image
+export { default as ProfileImage } from '../../public/images/profile.jpeg'
