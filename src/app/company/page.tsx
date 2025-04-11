8b44dce9eabcec1a234e5e8864afeaec2345d1ef
kwakseongjae <gkffhdnls13@gmail.com>
2025-04-11 18:25:24 +0900

    feat: Create new pages for account, company, guide, inquiry, and position sections

---
+++ src/app/company/page.tsx
@@ -0,0 +1,7 @@
+export default function CompanyPage() {
+  return (
+    <div>
+      <h1>기업 분석 프롬프트를 제공하는 페이지입니다.</h1>
+    </div>
+  )
+}
---
+++ src/app/position/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+      <h1>채용 공고 별 프롬프트 제공 페이지입니다.</h1>
+    </div>
+  )
+}
