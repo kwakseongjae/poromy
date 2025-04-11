6ca51dca463eb0b4589eb2e48d4a96b2bc992e5e
kwakseongjae <gkffhdnls13@gmail.com>
2025-04-11 18:48:36 +0900

    feat: Add ExclamationIcon SVG and update asset exports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,7 @@
 // SVG
 export { default as LogoIcon } from '../../public/svg/logo-icon.svg'
 export { default as ChevronIcon } from '../../public/svg/chevron-icon.svg'
+export { default as ExclamationIcon } from '../../public/svg/exclamation-icon.svg'
 
 // Image
 export { default as ProfileImage } from '../../public/images/profile.jpeg'
