9af42e27afac577a127854771fe94524d3612a69
kwakseongjae <gkffhdnls13@gmail.com>
2025-04-14 18:34:01 +0900

    feat: Add LinkIcon SVG and export it in assets index

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,6 +2,7 @@
 export { default as LogoIcon } from '../../public/svg/logo-icon.svg'
 export { default as ChevronIcon } from '../../public/svg/chevron-icon.svg'
 export { default as ExclamationIcon } from '../../public/svg/exclamation-icon.svg'
+export { default as LinkIcon } from '../../public/svg/link-icon.svg'
 
 // Image
 export { default as ProfileImage } from '../../public/images/profile.jpeg'
