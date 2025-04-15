b6d21b7d42157d8ead51f6f4ac5508d6eb9ca6db
kwakseongjae <gkffhdnls13@gmail.com>
2025-04-15 21:10:44 +0900

    feat: Add SVG icons and update asset exports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,6 +3,10 @@ export { default as ChevronIcon } from './icons/ChevronIcon'
 export { default as ExclamationIcon } from './icons/ExclamationIcon'
 export { default as LinkIcon } from './icons/LinkIcon'
 export { default as LogoIcon } from './icons/LogoIcon'
+export { default as HamburgerIcon } from './icons/HamburgerIcon'
+export { default as BulbIcon } from './icons/BulbIcon'
+export { default as DisabledBulbIcon } from './icons/DisabledBulbIcon'
+export { default as DisabledHamburgerIcon } from './icons/DisabledHamburgerIcon'
 
 // Image
 export { default as ProfileImage } from '../../public/images/profile.jpeg'
