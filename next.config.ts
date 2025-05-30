2444af086e3d51f813507dbb6aa98837b138083d
kwakseongjae <gkffhdnls13@gmail.com>
2025-05-31 03:40:45 +0900

    feat: Update Next.js configuration for enhanced image handling
    - Added support for AVIF and WebP formats to optimize image loading.
    - Configured device sizes and image sizes for better responsiveness.
    - Set minimum cache TTL to 1 year for improved caching strategy.
    - Enabled SVG support and specified content security policy for enhanced security.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     domains: [],
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60 * 60 * 24 * 365,
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   webpack(config) {
     // Configuration to convert SVG files to React components
